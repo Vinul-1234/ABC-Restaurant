369a3d5d72ab38534d96622e7b9a9bb1ed976142
H.T Hirushan Vinul <hirushan.vinul@gmail.com>
2024-09-07 09:58:52 +0530

    Update Header.tsx
    Changed Website Name and colour

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,10 +6,10 @@ const Header = () => {
   const { isLoggedIn } = useAppContext();
 
   return (
-    <div className="bg-blue-800 py-6">
+    <div className="bg-red-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to="/">Pujith Restuarant</Link>
+          <Link to="/">ABC Restuarant</Link>
         </span>
         <span className="flex space-x-2">
           {isLoggedIn ? (
